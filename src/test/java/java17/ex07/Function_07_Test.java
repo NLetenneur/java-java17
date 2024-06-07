4548803076d87a2d0f0699a98637dc56dbea447f
Noémie Letenneur <n.letenneur@gmail.com>
2024-06-07 17:52:33 +0200

    ex07 fini

--- src/test/java/java17/ex07/Function_07_Test.java
+++ src/test/java/java17/ex07/Function_07_Test.java
@@ -13,13 +13,19 @@ public class Function_07_Test {
     // TODO compléter la méthode pour qu'elle renvoie une chaîne de caractères de la forme "(<nb1><symbol><nb2>)=<resultat>"
     // TODO ex. "(10+11)=21", "(5-2)=3"
     String format(int nb1, int nb2, String symbol, IntBinaryOperator operator) {
-        // TODO
-        return null;
+    	int resultat = operator.applyAsInt(nb1, nb2);
+    	return "("+nb1+""+symbol+""+nb2+")="+resultat;
     }
     // end::format[]
 
     // TODO définir sum pour que le test test_format_sum() soit passant
-    IntBinaryOperator sum = null;
+    IntBinaryOperator sum = new IntBinaryOperator() {
+
+		@Override
+		public int applyAsInt(int left, int right) {
+			return left+right;
+		}
+    };
 
     @Test
     public void test_format_sum() throws Exception {
@@ -30,7 +36,13 @@ public class Function_07_Test {
     }
 
     // TODO définir substract afin que le test test_format_subtract() soit passant
-    IntBinaryOperator substract = null;
+    IntBinaryOperator substract = new IntBinaryOperator() {
+
+		@Override
+		public int applyAsInt(int left, int right) {
+			return left-right;
+		}
+    };
 
     @Test
     public void test_format_subtract() throws Exception {
