de3f6a2b6bac78166d844b7da8754c76cba822ed
Noémie Letenneur <n.letenneur@gmail.com>
2024-06-07 17:33:49 +0200

    ex 05 fini

--- src/test/java/java17/ex05/Function_05_Test.java
+++ src/test/java/java17/ex05/Function_05_Test.java
@@ -1,5 +1,7 @@
 package java17.ex05;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import java.util.List;
 import java.util.function.Consumer;
 
@@ -16,15 +18,36 @@ public class Function_05_Test {
     //tag::functions[]
     // TODO compléter la fonction
     // TODO modifier le mot de passe en "secret"
-    Consumer<Person> changePasswordToSecret = null;
+    Consumer<Person> changePasswordToSecret = new Consumer<>() {
+
+		@Override
+		public void accept(Person t) {
+			t.setPassword("secret");
+		}
+		
+    	
+    };
 
     // TODO compléter la fonction
     // TODO vérifier que l'age > 4 avec une assertion JUnit
-    Consumer<Person> verifyAge = null;
+    Consumer<Person> verifyAge = new Consumer<>() {
+
+		@Override
+		public void accept(Person t) {
+			assertTrue(t.getAge() >=4);			
+		}
+    	
+    };
 
     // TODO compléter la fonction
     // TODO vérifier que le mot de passe est "secret" avec une assertion JUnit
-    Consumer<Person> verifyPassword = null;
+    Consumer<Person> verifyPassword = new Consumer<>() {
+
+		@Override
+		public void accept(Person t) {
+			assertTrue(t.getPassword().equals("secret"));			
+		}
+    };
     //end::functions[]
 
 
@@ -34,10 +57,12 @@ public class Function_05_Test {
 
         // TODO invoquer la méthode personList.forEach pour modifier les mots de passe en "secret"
         // personList.forEach...
+        personList.forEach(changePasswordToSecret);
 
         // TODO remplacer la boucle for par l'invocation de la méthode forEach
         // TODO Utiliser la méthode andThen pour chaîner les vérifications verifyAge et verifyPassword
-        // personList.forEach...
+        personList.forEach(verifyAge);
+        personList.forEach(verifyPassword);
         for(Person p : personList) {
             verifyAge.accept(p);
             verifyPassword.accept(p);
